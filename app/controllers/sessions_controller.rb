60d3ec2c17ae5e9f0c2d6362893e9147a77900b4
Chandra Shekhar <shekharindian099@gmail.com>
2016-01-07 19:00:09 +0530

    Access limited for non-login users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,16 @@
 class SessionsController < ApplicationController
+  skip_before_action :authorize, only: [:new, :create]
   def new
   end
 
   def create
   	user = User.find_by(name: params[:name])
   	if user and user.authenticate(params[:password])
-  		session[:user_id] = user.user_id
+  		session[:user_id] = user.id
   		redirect_to admin_url
   	else
-  		redirect_to login_url, alert: "Invalid user/password combination"
-  	end
+  		render "new", alert: "Invalid user/password combination"
+    end
   end
 
   def destroy
