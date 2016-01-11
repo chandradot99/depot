07536e568369bcab06865b4825aafcb501d3697b
Chandra Shekhar <shekharindian099@gmail.com>
2016-01-11 18:04:25 +0530

    Internationalization Added

--- config/routes.rb
+++ config/routes.rb
@@ -7,16 +7,20 @@ Rails.application.routes.draw do
     delete 'logout' => :destroy
   end
 
+  get "sessions/create"
+  get "sessions/destroy"
+
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  get 'store/index'
 
   resources :products do
   	get :who_bought, on: :member
   end
 
-  root 'store#index', as: 'store'
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
   
 end
